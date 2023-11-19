b3036d8b713d9946c460b9dfffa5ceda03b75b97
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-19 10:29:47 +0100

    operatory przypisania - update

---
+++ operatory-przypisania.py
@@ -0,0 +1,21 @@
+x = 0
+print(x)
+
+x = x + 1
+x += 10  # >> x = x + 1
+print(x)
+
+x -= 1  # >> x = x - 1
+print(x)
+
+x *= 2  # >> x = x * 2
+print(x)
+x /= 5  # >> x = x / 5
+print(x)
+x %= 2  # >> x = x % 2 >> reszta z dzielenia przez 2
+print(x)
+x = 15
+x //= 2  # >> x = x // 2 >> dzielenie bez reszty
+print(x)
+x **= 3  # >> x = x ** 3 >> potęgowanie
+print(x)
